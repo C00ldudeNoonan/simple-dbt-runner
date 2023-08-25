5a4f6e31c3941a22a6e9395765484341f9bf4ece
Dani Mermelstein <dani@withsylva.com>
2023-08-25 14:51:49 +0800

    we update profiles.yml and also the requirements.txt with whatever adapter is needed for the user's DWH choice

--- update_profile_with_prod.py
+++ update_profile_with_prod.py
@@ -14,20 +14,65 @@ with open('project_goes_here/dbt_project.yml', 'r') as f:
 dwh_type = sys.argv[1]
 print("dwh_type: ", dwh_type)
 
-# Add a new target called "prod"
-profiles[profile_name]['outputs']['prod'] = {
-    'type': 'postgres',
-    'host': '{{ env_var("HOST") }}',
-    'user': '{{ env_var("PROD_USERNAME") }}',
-    'password': '{{ env_var("PROD_PASSWORD") }}',
-    'port': '{{ env_var("PORT") | int }}',
-    'dbname': '{{ env_var("DATABASE_NAME") }}',
-    'schema': '{{ env_var("PROD_SCHEMA") }}',
-    'threads': 8
+# Add a new target called "prod" based on the type of datawarehouse
+if dwh_type == 'postgres':
+    profiles[profile_name]['outputs']['prod'] = {
+        'type': 'postgres',
+        'host': '{{ env_var("HOST") }}',
+        'user': '{{ env_var("PROD_USERNAME") }}',
+        'password': '{{ env_var("PROD_PASSWORD") }}',
+        'port': '{{ env_var("PORT") | int }}',
+        'dbname': '{{ env_var("DATABASE_NAME") }}',
+        'schema': '{{ env_var("PROD_SCHEMA") }}',
+        'threads': 8
+    }
+elif dwh_type == 'snowflake':
+    profiles[profile_name]['outputs']['prod'] = {
+        'type': 'snowflake',
+        'account': '{{ env_var("SNOWFLAKE_ACCOUNT") }}',
+        'role': '{{ env_var("SNOWFLAKE_ROLE") }}',
+        'user': '{{ env_var("PROD_USERNAME") }}',
+        'password': '{{ env_var("PROD_PASSWORD") }}',
+        'database': '{{ env_var("DATABASE_NAME") }}',
+        'schema': '{{ env_var("PROD_SCHEMA") }}',
+        'warehouse': '{{ env_var("PROD_WAREHOUSE") }}',
+        'threads': 8
+    }
+elif dwh_type == 'redshift':
+    profiles[profile_name]['outputs']['prod'] = {
+        'type': 'redshift',
+        'host': '{{ env_var("HOST") }}',
+        'user': '{{ env_var("PROD_USERNAME") }}',
+        'password': '{{ env_var("PROD_PASSWORD") }}',
+        'port': '{{ env_var("PORT") | int }}',
+        'dbname': '{{ env_var("DATABASE_NAME") }}',
+        'schema': '{{ env_var("PROD_SCHEMA") }}',
+        'threads': 8
+    }
+elif dwh_type == 'bigquery':
+    profiles[profile_name]['outputs']['prod'] = {
+        'type': 'bigquery',
+        'method': 'oauth',
+        'project': '{{ env_var("PROJECT_NAME") }}',
+        'dataset': '{{ env_var("PROD_DATASET") }}',
+        'threads': 8
 }
 
 # Save the updated profiles.yml file
 with open('project_goes_here/profiles.yml', 'w') as f:
     yaml.dump(profiles, f)
 
-print(profiles)
+# update requirements.txt with the dbt adapter depending on the type of datawarehouse
+if dwh_type == 'postgres':
+    with open('requirements.txt', 'a') as f:
+        f.write("dbt-postgres==1.5.2")
+elif dwh_type == 'snowflake':
+    with open('requirements.txt', 'a') as f:
+        f.write("dbt-snowflake==1.5.3")
+elif dwh_type == 'redshift':
+    with open('requirements.txt', 'a') as f:
+        f.write("dbt-redshift==1.6.1")
+elif dwh_type == 'bigquery':
+    with open('requirements.txt', 'a') as f:
+        f.write("dbt-bigquery==1.6.3")
+
