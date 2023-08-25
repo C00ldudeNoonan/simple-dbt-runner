3762b672a777d6de9739ef50a231d61bac7a4a80
Dani Mermelstein <dani@withsylva.com>
2023-08-25 14:03:27 +0800

    wip

--- update_profile_with_prod.py
+++ update_profile_with_prod.py
@@ -1,3 +1,4 @@
+import sys
 import yaml
 
 # Load the profiles.yml file
@@ -7,8 +8,12 @@ with open('project_goes_here/profiles.yml', 'r') as f:
 # Get the name of the first profile in the file
 profile_name = list(profiles.keys())[0]
 
+# Get the type of datawarehouse we are using
+dwh_type = sys.argv[1]
+print(dwh_type)
+
 # Add a new target called "prod"
-profiles[profile_name]['outputs']['prod'] = {
+profiles[[profile_name]]['outputs']['prod'] = {
     'type': 'postgres',
     'host': '{{ env_var("HOST") }}',
     'user': '{{ env_var("PROD_USERNAME") }}',
