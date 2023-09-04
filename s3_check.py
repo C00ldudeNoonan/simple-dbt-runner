6e22f21f87778a827eca98a9ea6996ba70a005c8
Dani Mermelstein <dani@withsylva.com>
2023-09-04 14:38:19 +0800

    fix

--- s3_check.py
+++ s3_check.py
@@ -3,7 +3,8 @@ import boto3
 
 s3 = boto3.client('s3')
 
-bucket = os.environ.get('AWS_S3_BUCKET')
+# bucket = os.environ.get('AWS_S3_BUCKET')
+bucket = 'dbt-s3bucket-eywrs70vvmxj'
 
 response = s3.list_objects_v2(Bucket=bucket)
 
