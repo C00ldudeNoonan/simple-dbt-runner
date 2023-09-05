9feb46db636d2f766e6fe2b5784bde5459771634
Dani Mermelstein <dani@withsylva.com>
2023-09-05 17:33:53 +0800

    automating env var updates on all workflow files

--- update_profile_with_prod.py
+++ update_profile_with_prod.py
@@ -1,90 +1,171 @@
 import sys
-import yaml
+# import yaml
 import ruamel.yaml
 
 yaml = ruamel.yaml.YAML()
 yaml.preserve_quotes = True
 
-# # Load the profiles.yml file
-# with open('project_goes_here/profiles.yml', 'r') as f:
-#     profiles = yaml.safe_load(f)
+workflow_jobs = {
+    'run_dbt.yml': 'run_dbt',
+    'run_dbt_force.yml': 'run_dbt',
+    'run_dbt_on_cron.yml': 'dbt_scheduled_run',
+    'run_dbt_on_merge.yml': 'dbt_run_on_merge',
+    'run_incremental_dbt_on_merge.yml': 'dbt_run_on_merge_incremental',
+    'run_dbt_on_pr_dev' : 'dbt_run_on_pr'
+}
 
-# # get the profile name from dbt_project.yml
-# with open('project_goes_here/dbt_project.yml', 'r') as f:
-#     dbt_project = yaml.safe_load(f)
-#     profile_name = dbt_project['profile']
+# Load the profiles.yml file
+with open('project_goes_here/profiles.yml', 'r') as f:
+    profiles = yaml.load(f)
 
-# # Get the type of datawarehouse we are using
-# dwh_type = sys.argv[1]
-# print("dwh_type: ", dwh_type)
+# get the profile name from dbt_project.yml
+with open('project_goes_here/dbt_project.yml', 'r') as f:
+    dbt_project = yaml.load(f)
+    profile_name = dbt_project['profile']
 
-# # Add a new target called "prod" based on the type of datawarehouse
-# if dwh_type == 'postgres':
-#     profiles[profile_name]['outputs']['prod'] = {
-#         'type': 'postgres',
-#         'host': '{{ env_var("HOST") }}',
-#         'user': '{{ env_var("PROD_USERNAME") }}',
-#         'password': '{{ env_var("PROD_PASSWORD") }}',
-#         'port': '{{ env_var("PORT") | int }}',
-#         'dbname': '{{ env_var("DATABASE_NAME") }}',
-#         'schema': '{{ env_var("PROD_SCHEMA") }}',
-#         'threads': 8
-#     }
-# elif dwh_type == 'snowflake':
-#     profiles[profile_name]['outputs']['prod'] = {
-#         'type': 'snowflake',
-#         'account': '{{ env_var("SNOWFLAKE_ACCOUNT") }}',
-#         'role': '{{ env_var("SNOWFLAKE_ROLE") }}',
-#         'user': '{{ env_var("PROD_USERNAME") }}',
-#         'password': '{{ env_var("PROD_PASSWORD") }}',
-#         'database': '{{ env_var("DATABASE_NAME") }}',
-#         'schema': '{{ env_var("PROD_SCHEMA") }}',
-#         'warehouse': '{{ env_var("PROD_WAREHOUSE") }}',
-#         'threads': 8
-#     }
-# elif dwh_type == 'redshift':
-#     profiles[profile_name]['outputs']['prod'] = {
-#         'type': 'redshift',
-#         'host': '{{ env_var("HOST") }}',
-#         'user': '{{ env_var("PROD_USERNAME") }}',
-#         'password': '{{ env_var("PROD_PASSWORD") }}',
-#         'port': '{{ env_var("PORT") | int }}',
-#         'dbname': '{{ env_var("DATABASE_NAME") }}',
-#         'schema': '{{ env_var("PROD_SCHEMA") }}',
-#         'threads': 8
-#     }
-# elif dwh_type == 'bigquery':
-#     profiles[profile_name]['outputs']['prod'] = {
-#         'type': 'bigquery',
-#         'method': 'oauth',
-#         'project': '{{ env_var("PROJECT_NAME") }}',
-#         'dataset': '{{ env_var("PROD_DATASET") }}',
-#         'threads': 8
-# }
+# Get the type of datawarehouse we are using
+dwh_type = sys.argv[1]
+print("dwh_type: ", dwh_type)
 
-# # Save the updated profiles.yml file
-# with open('project_goes_here/profiles.yml', 'w') as f:
-#     yaml.dump(profiles, f)
+# Add a new target called "prod" based on the type of datawarehouse
+if dwh_type == 'postgres':
+    profiles[profile_name]['outputs']['prod'] = {
+        'type': 'postgres',
+        'host': '{{ env_var("HOST") }}',
+        'user': '{{ env_var("USERNAME") }}',
+        'password': '{{ env_var("PASSWORD") }}',
+        'port': '{{ env_var("PORT") | int }}',
+        'dbname': '{{ env_var("DATABASE") }}',
+        'schema': '{{ env_var("SCHEMA") }}',
+        'threads': 8
+    }
 
-# # update requirements.txt with the dbt adapter depending on the type of datawarehouse
-# if dwh_type == 'postgres':
-#     with open('requirements.txt', 'a') as f:
-#         f.write("\ndbt-postgres==1.5.2")
-# elif dwh_type == 'snowflake':
-#     with open('requirements.txt', 'a') as f:
-#         f.write("\ndbt-snowflake==1.5.3")
-# elif dwh_type == 'redshift':
-#     with open('requirements.txt', 'a') as f:
-#         f.write("\ndbt-redshift==1.6.1")
-# elif dwh_type == 'bigquery':
-#     with open('requirements.txt', 'a') as f:
-#         f.write("\ndbt-bigquery==1.6.3")
+    # update the gh actions jobs
+    for file, job in workflow_jobs.items():
+        # read the run_dbt.yml file
+        with open('.github/workflows/'+file, 'r') as f:
+            data = yaml.load(f)
 
-# update the gh actions job
-# read the run_dbt.yml file
-with open('.github/workflows/run_dbt.yml', 'r') as f:
-    data = yaml.load(f)
+        # edit the env block for all the jobs
+        data['jobs'][job]['env'] = {
+            'HOST': '${{ secrets.HOST }}',
+            'DATABASE': '${{ secrets.DATABASE }}',
+            'USERNAME': '${{ secrets.USERNAME }}',
+            'PASSWORD': '${{ secrets.PASSWORD }}',
+            'PORT': '${{ secrets.PORT }}',
+            'SCHEMA': '${{ secrets.SCHEMA }}'
+        }
 
-# write the updated run_dbt.yml file
-with open('.github/workflows/run_dbt.yml', 'w') as f:
-    yaml.dump(data, f)
+        # write the updated run_dbt.yml file
+        with open('.github/workflows/'+file, 'w') as f:
+            yaml.dump(data, f)
+
+elif dwh_type == 'snowflake':
+    profiles[profile_name]['outputs']['prod'] = {
+        'type': 'snowflake',
+        'account': '{{ env_var("SNOWFLAKE_ACCOUNT") }}',
+        'role': '{{ env_var("SNOWFLAKE_ROLE") }}',
+        'user': '{{ env_var("USERNAME") }}',
+        'password': '{{ env_var("PASSWORD") }}',
+        'database': '{{ env_var("DATABASE") }}',
+        'schema': '{{ env_var("SCHEMA") }}',
+        'warehouse': '{{ env_var("WAREHOUSE") }}',
+        'threads': 8
+    }
+
+    # update the gh actions jobs
+    for file, job in workflow_jobs.items():
+        # read the run_dbt.yml file
+        with open('.github/workflows/'+file, 'r') as f:
+            data = yaml.load(f)
+
+        # edit the env block for all the jobs
+        data['jobs'][job]['env'] = {
+            'SNOWFLAKE_ACCOUNT': '${{ secrets.SNOWFLAKE_ACCOUNT }}',
+            'DATABASE': '${{ secrets.DATABASE }}',
+            'SNOWFLAKE_ROLE': '${{ secrets.SNOWFLAKE_ROLE }}',
+            'USERNAME': '${{ secrets.USERNAME }}',
+            'PASSWORD': '${{ secrets.PASSWORD }}',
+            'SCHEMA': '${{ secrets.SCHEMA }}',
+            'WAREHOUSE': '${{ secrets.WAREHOUSE }}',
+        }
+
+        # write the updated run_dbt.yml file
+        with open('.github/workflows/'+file, 'w') as f:
+            yaml.dump(data, f)
+
+elif dwh_type == 'redshift':
+    profiles[profile_name]['outputs']['prod'] = {
+        'type': 'redshift',
+        'host': '{{ env_var("HOST") }}',
+        'user': '{{ env_var("USERNAME") }}',
+        'password': '{{ env_var("PASSWORD") }}',
+        'port': '{{ env_var("PORT") | int }}',
+        'dbname': '{{ env_var("DATABASE") }}',
+        'schema': '{{ env_var("SCHEMA") }}',
+        'threads': 8
+    }
+
+    # update the gh actions jobs
+    for file, job in workflow_jobs.items():
+        # read the run_dbt.yml file
+        with open('.github/workflows/'+file, 'r') as f:
+            data = yaml.load(f)
+
+        # edit the env block for all the jobs
+        data['jobs'][job]['env'] = {
+            'HOST': '${{ secrets.HOST }}',
+            'USERNAME': '${{ secrets.USERNAME }}',
+            'PASSWORD': '${{ secrets.PASSWORD }}',
+            'PORT': '${{ secrets.PORT }}',
+            'DATABASE': '${{ secrets.DATABASE }}',
+            'SCHEMA': '${{ secrets.SCHEMA }}',
+        }
+
+        # write the updated run_dbt.yml file
+        with open('.github/workflows/'+file, 'w') as f:
+            yaml.dump(data, f)
+
+elif dwh_type == 'bigquery':
+    profiles[profile_name]['outputs']['prod'] = {
+        'type': 'bigquery',
+        'method': 'oauth',
+        'project': '{{ env_var("PROJECT_NAME") }}',
+        'dataset': '{{ env_var("DATASET") }}',
+        'threads': 8
+    }
+
+
+            # update the gh actions jobs
+    for file, job in workflow_jobs.items():
+        # read the run_dbt.yml file
+        with open('.github/workflows/'+file, 'r') as f:
+            data = yaml.load(f)
+
+        # edit the env block for all the jobs
+        data['jobs'][job]['env'] = {
+            'PROJECT_NAME': '${{ secrets.PROJECT_NAME }}',
+            'DATASET': '${{ secrets.DATASET }}',
+        }
+
+        # write the updated run_dbt.yml file
+        with open('.github/workflows/'+file, 'w') as f:
+            yaml.dump(data, f)
+
+# Save the updated profiles.yml file
+with open('project_goes_here/profiles.yml', 'w') as f:
+    yaml.dump(profiles, f)
+
+# update requirements.txt with the dbt adapter depending on the type of datawarehouse
+if dwh_type == 'postgres':
+    with open('requirements.txt', 'a') as f:
+        f.write("\ndbt-postgres==1.5.2")
+elif dwh_type == 'snowflake':
+    with open('requirements.txt', 'a') as f:
+        f.write("\ndbt-snowflake==1.5.3")
+elif dwh_type == 'redshift':
+    with open('requirements.txt', 'a') as f:
+        f.write("\ndbt-redshift==1.6.1")
+elif dwh_type == 'bigquery':
+    with open('requirements.txt', 'a') as f:
+        f.write("\ndbt-bigquery==1.6.3")
