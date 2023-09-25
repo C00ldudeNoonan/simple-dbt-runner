ff88668fbe236f15277e9e597a8b69f6c6417c6b
Dani Mermelstein <dani@withsylva.com>
2023-09-24 20:30:36 -0400

    just try to checkout gh-pages

--- get_manifest.sh
+++ get_manifest.sh
@@ -2,17 +2,18 @@
 set -e
 
 git fetch
+git checkout gh-pages
 
-# try to get the manifest from the gh-pages branch
-if ! git checkout gh-pages manifest.json; then
-  echo "Failed to get manifest.json from gh-pages branch"
-  exit 1
-fi
+# # try to get the manifest from the gh-pages branch
+# if ! git checkout gh-pages manifest.json; then
+#   echo "Failed to get manifest.json from gh-pages branch"
+#   exit 1
+# fi
 
-# create the target directory if it doesn't exist
-if [ ! -d "project_goes_here/target" ]; then
-  mkdir project_goes_here/target
-fi
+# # create the target directory if it doesn't exist
+# if [ ! -d "project_goes_here/target" ]; then
+#   mkdir project_goes_here/target
+# fi
 
-# copy the manifest.json to the target directory
-cp manifest.json project_goes_here/target/manifest.json
+# # copy the manifest.json to the target directory
+# cp manifest.json project_goes_here/target/manifest.json
