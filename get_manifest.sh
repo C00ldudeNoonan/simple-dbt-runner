0498be792a150346c4f5a925ff0a2aa5e3cb878a
Dani Mermelstein <dani@withsylva.com>
2023-09-26 12:08:40 -0400

    saving previous manifest to different directory

--- get_manifest.sh
+++ get_manifest.sh
@@ -6,10 +6,10 @@ git fetch
 # try to get the manifest from the gh-pages branch
 git checkout origin/gh-pages manifest.json
 
-# create the target directory if it doesn't exist
-if [ ! -d "project_goes_here/target" ]; then
-  mkdir project_goes_here/target
+# create the prev_run_state directory if it doesn't exist
+if [ ! -d "project_goes_here/prev_run_state" ]; then
+  mkdir project_goes_here/prev_run_state
 fi
 
 # copy the manifest.json to the target directory
-cp manifest.json project_goes_here/target/manifest.json
+cp manifest.json project_goes_here/prev_run_state/manifest.json
