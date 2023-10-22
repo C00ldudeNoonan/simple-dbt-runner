051872e777aa2176eb3a104e813baac2c3f26907
Jay Ashe <jayashe211@gmail.com>
2023-10-22 16:10:06 -0400

    Remove missing workflow

--- update_profile_with_prod.py
+++ update_profile_with_prod.py
@@ -8,7 +8,6 @@ workflow_jobs = {
     'run_dbt.yml': 'run_dbt',
     'run_dbt_force.yml': 'run_dbt',
     'run_dbt_on_cron.yml': 'dbt_scheduled_run',
-    'run_dbt_on_merge.yml': 'dbt_run_on_merge',
     'run_incremental_dbt_on_merge.yml': 'dbt_run_on_merge_incremental',
     'run_dbt_on_pr' : 'dbt_run_on_pr',
     'run_dbt_cleanup' : 'dbt_run_cleanup'
